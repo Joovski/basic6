ee2212c4cc0d816bde3e75614fb978f4d6745a43
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-23 17:26:52 +0100

    Create cross.lua

---
+++ cross.lua
@@ -0,0 +1,8 @@
+	circle = Drawing.new("Circle")
+	circle.Position = Vector2.new(workspace.CurrentCamera.ViewportSize.X / 2, workspace.CurrentCamera.ViewportSize.Y / 2)
+	circle.Color = Color3.fromRGB(255, 0, 0)
+	circle.Thickness = 0.1
+	circle.NumSides = 12
+	circle.Radius = 3
+	circle.Visible = true
+	circle.Filled = true
