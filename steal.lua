d223b46c404ef42f8e9167b230b295a462e7ab93
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-23 17:25:40 +0100

    Create steal.lua

---
+++ steal.lua
@@ -0,0 +1,14 @@
+pcall(function()
+		char = game:GetService('Players').LocalPlayer.Character
+		game:GetService('Players').LocalPlayer.CharacterAdded:connect(function(body)
+			char = body end)
+		for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA('Tool') then
+				char.Humanoid:EquipTool(v) end end
+		workspace.DescendantAdded:connect(function(item)
+			pcall(function()
+				if item:IsA('Tool') then 
+				end 
+			end) 
+		end)
+end);
